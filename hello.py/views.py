1daf6c93b9481ec5b4c057697df2e46e44064037
Vineesh VJ <105381041+vineeshvj@users.noreply.github.com>
2023-03-15 13:30:41 +0530

    Add files via upload

---
+++ hello.py/views.py
@@ -0,0 +1,28 @@
+from  flask import Blueprint, render_template,  request, jsonify, redirect, url_for
+
+views = Blueprint(__name__ ,"views")
+
+
+@views.route("/")
+def home():
+    return render_template("index.html", name="jeo")
+
+@views.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@views.route("/json")
+def get_json():
+    return jsonify({'name': 'tim', 'coolness':10})
+
+
+
+
+@views.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+@views.route("/go-to-home")
+def go_to_home():
+    return redirect(url_for("views.home"))
